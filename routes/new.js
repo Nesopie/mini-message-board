cf09d1e6066047e0173ebd4a5787f953dfbb0b4d
Ayman <yaoiotakusama@gmail.com>
2021-10-19 17:38:29 +0530

    fixed the path to the new message

--- routes/new.js
+++ routes/new.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res,next) => {
+    console.log(req);
     res.render('new', {
         title: 'Create a new message!'
     });
